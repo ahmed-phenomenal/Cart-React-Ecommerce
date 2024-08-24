480de21682cb988129e9bade14692e02edc46975
unknown <ahmedtamer18102003@gmail.com>
2024-08-24 13:49:31 +0300

    first commit

--- src/API/payment.js
+++ src/API/payment.js
@@ -3,7 +3,7 @@ import axios from "axios";
 let token = localStorage.getItem('userToken')
 
 export function onlinepayment({cartId,shippingAddress}){
-    return axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/66c64bd8ed0dc0016ceb5d79?url=http://localhost:5174`,{shippingAddress},{headers:{token}})
+    return axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/66c64bd8ed0dc0016ceb5d79?url=https://cart-react-ecommerce.vercel.app/`,{shippingAddress},{headers:{token}})
 }
 
 export function Cash({cartId,shippingAddress}){
